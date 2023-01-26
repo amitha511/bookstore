7497d99e586d5185e70847d29e875f4bcb0d7795
Maya Keren <101181197+MayaKeren1@users.noreply.github.com>
2023-01-26 11:44:23 +0200

    update1

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -3,25 +3,37 @@ import mongoose from 'mongoose';
 const productSchema = new mongoose.Schema(
   {
     amount: {
-        type: Number,
-        require: true,
-        min:0
+      type: Number,
+      require: true,
+      min: 0
     },
     img: {
-        type: String,
-        require: true
+      type: String,
+      require: true
     },
     name: {
-        type: String,
-        require: true
+      type: String,
+      require: true
     },
     genre: {
-        type: String,
-        require: true
+      type: String,
+      require: true
     },
     store: {
-        type: String,
-        require: true
+      type: String,
+      require: true
+    },
+    language: {
+      type: String,
+      require: true
+    },
+    pages: {
+      type: String,
+      require: true
+    },
+    year: {
+      type: String,
+      require: true
     }
   },
   {
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React,{ useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import axios from 'axios';
 import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
@@ -31,16 +31,25 @@ function HomeScreen() {
   });
   const [genreSearch, setGenreSearch] = useState("/all");
   const [storeSearch, setStoreSearch] = useState("/all");
+  const [priceSearch, setPriceSearch] = useState("/all");
+  const [lenguageSearch, setLanguageSearch] = useState("/all");
+  const [pagesSearch, setPagesSearch] = useState("/all");
+  const [yearSearch, setYearSearch] = useState("/all");
+  const [routingSearch, setRoutingSearch] = useState(0);
+  useEffect(() => {
 
-
-    useEffect(() => {
-    
-      const fetchData = async () => {
+    const fetchData = async () => {
       dispatch({ type: 'FETCH_REQUEST' });
       try {
-        console.log('/api/products/search'+ genreSearch + storeSearch);
-        let result = await axios.get('/api/products/search'+ genreSearch + storeSearch);
-        
+        let result
+        if (routingSearch == 0) {
+          result = await axios.get('/api/products/search' + genreSearch + storeSearch + priceSearch);
+        }
+        else {
+          console.log(lenguageSearch);
+          result = await axios.get('/api/products/searchSecond' + lenguageSearch + pagesSearch + yearSearch);
+        }
+
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
         dispatch({ type: 'FETCH_FAIL', payload: err.message });
@@ -48,49 +57,122 @@ function HomeScreen() {
 
     };
 
-     fetchData();
-   },[genreSearch , storeSearch]);
+    fetchData();
+  }, [genreSearch, storeSearch, priceSearch, lenguageSearch, pagesSearch, yearSearch]);
 
 
   const genre = (e) => {
-    if(e != null)
-      setGenreSearch('/'+ e);
+    if (e != null) {
+      setGenreSearch('/' + e);
+      setRoutingSearch(0);
+    }
     else
       setGenreSearch("")
-    
-    console.log("genre: " + genreSearch);
-
   }
 
   const store = (e) => {
-    if (e != null)
+    if (e != null) {
       setStoreSearch("/" + e);
+      setRoutingSearch(0);
+    }
     else
       setStoreSearch("");
-    console.log("store: "+storeSearch);
   }
- 
+
+  const price = (e) => {
+    if (e != null) {
+      setPriceSearch("/" + e);
+      setRoutingSearch(0);
+    }
+    else
+      setPriceSearch("");
+  }
+
+  const language = (e) => {
+    if (e != null) {
+      setLanguageSearch('/' + e);
+      setRoutingSearch(1);
+    }
+    else
+      setLanguageSearch("")
+  }
+
+  const pages = (e) => {
+    if (e != null) {
+      setPagesSearch("/" + e);
+      setRoutingSearch(1);
+    }
+    else
+      setPagesSearch("");
+  }
+
+  const year = (e) => {
+    if (e != null) {
+      setYearSearch("/" + e);
+      setRoutingSearch(1);
+    }
+    else
+      setYearSearch("");
+  }
+
   return (
     <div>
       <Helmet>
         <title>Amazon best seller</title>
       </Helmet>
       <h1>Our Books</h1>
+      <p>
         <label for="genre">Genre: </label>
-        <select name="genre" id="genre" onChange={(event) => {genre(event.target.value);}}>
+        <select name="genre" id="genre" onChange={(event) => { genre(event.target.value); }}>
           <option value="all">all</option>
           <option value="fantasy">Fantasy</option>
           <option value="romantic">Romantic</option>
           <option value="action">Action</option>
-      </select>
-      
+        </select>
+
         <label for="store">Store: </label>
-        <select name="store" id="store" onChange={(event) => {store(event.target.value);}}>
+        <select name="store" id="store" onChange={(event) => { store(event.target.value); }}>
           <option value="all">all</option>
           <option value="stimatsky">Stimatsky</option>
           <option value="amazon">Amazon</option>
           <option value="zomet sfarim">Zomet Sfarim</option>
         </select>
+
+        <label for="amount">Price: </label>
+        <select name="amount" id="amount" onChange={(event) => { price(event.target.value); }}>
+          <option value="all">all</option>
+          <option value="9">9$</option>
+          <option value="8">8$</option>
+          <option value="10">10$</option>
+          <option value="12">12$</option>
+          <option value="14">14$</option>
+        </select>
+      </p>
+      <p>
+        <label for="language">Language: </label>
+        <select name="language" id="language" onChange={(event) => { language(event.target.value); }}>
+          <option value="all">all</option>
+          <option value="English">English</option>
+          <option value="Spanish">Spanish</option>
+          <option value="German">German</option>
+        </select>
+
+        <label for="pages">Pages: </label>
+        <select name="pages" id="pages" onChange={(event) => { pages(event.target.value); }}>
+          <option value="all">all</option>
+          <option value="335">335</option>
+          <option value="500">500</option>
+          <option value="384">384</option>
+        </select>
+
+        <label for="year">Year: </label>
+        <select name="year" id="year" onChange={(event) => { year(event.target.value); }}>
+          <option value="all">all</option>
+          <option value="2016">2016</option>
+          <option value="2022">2022</option>
+          <option value="2023">2023</option>
+        </select>
+      </p>
 
       {/* <h4>Search:</h4>
       <input type="search" placeholder="name..." onChange={(event) => {genre(event.target.value);}}/>
